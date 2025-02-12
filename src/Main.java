75fc0a3779b5c3ba67e39e10cd94cfc1fa81258d
Abram <tacksontiger@gmail.com>
2025-02-12 18:59:38 +0300

    commit #6
    in the NV on line number 4 and 5 i created a instances of StaticInitializationDemo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Static variable before creating any instances: " + StaticInitializationDemo.getStaticVariable());
+
+        StaticInitializationDemo instance1 = new StaticInitializationDemo("Instance 1");
+        StaticInitializationDemo instance2 = new StaticInitializationDemo("Instance 2");
+        
     }
 }
