93126e3ba9a61ac0abba803184b8bba1f3af8f5e
Abram <tacksontiger@gmail.com>
2025-02-12 19:05:58 +0300

    commit #8
    In the NV on line number 9 and 10 shows the out of the instance specific initialization

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         StaticInitializationDemo instance1 = new StaticInitializationDemo("Instance 1");
         StaticInitializationDemo instance2 = new StaticInitializationDemo("Instance 2");
         System.out.println("Static variable after creating instances: " + StaticInitializationDemo.getStaticVariable());
+
+        System.out.println("Instance 1 variable: " + instance1.getInstanceVariable());
+        System.out.println("Instance 2 variable: " + instance2.getInstanceVariable());
     }
 }
